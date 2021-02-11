df46a54a24da7388a73e015530f18e2a0f4f568a
Rémi Bastide <Remi.Bastide@gmail.com>
2021-02-11 15:41:49 +0100

    Postgres sur profil Heroku

--- src/main/java/galerie/controller/GalerieController.java
+++ src/main/java/galerie/controller/GalerieController.java
@@ -1,19 +1,11 @@
 package galerie.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import galerie.dao.GalerieRepository;
 import galerie.entity.Galerie;
-import java.util.List;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
@@ -23,8 +15,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping(path = "/galerie")
 public class GalerieController {
 
-    @Autowired
-    private GalerieRepository dao;
+    private final GalerieRepository dao;
+
+    public GalerieController(GalerieRepository dao) {
+        this.dao = dao;
+    }
 
     /**
      * Affiche toutes les catégories dans la base
--- src/main/java/galerie/controller/TableauController.java
+++ src/main/java/galerie/controller/TableauController.java
@@ -1,35 +1,28 @@
 package galerie.controller;
-/**
- * Edition des tableaux
- */
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import lombok.extern.log4j.Log4j2;
 
 import galerie.dao.ArtisteRepository;
 import galerie.dao.TableauRepository;
 import galerie.entity.Tableau;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2 // Génère le 'logger' pour afficher les messages de trace
 @Controller
 @RequestMapping(path = "/tableau")
 public class TableauController {
 
-    @Autowired
-    private TableauRepository tableauDAO;
+    private final TableauRepository tableauDAO;
 
-    @Autowired
-    private ArtisteRepository artisteDAO;
+    private final ArtisteRepository artisteDAO;
+
+    public TableauController(TableauRepository tableauDAO, ArtisteRepository artisteDAO) {
+        this.tableauDAO = tableauDAO;
+        this.artisteDAO = artisteDAO;
+    }
 
     /**
      * Affiche toutes les tableaux dans la base
@@ -50,6 +43,8 @@ public class TableauController {
      */
     @GetMapping(path = "add")
     public String montreLeFormulairePourAjout(@ModelAttribute("tableau") Tableau tableau, Model model) {
+        // Tableau tableau = new Tableau();
+        // model.addAttribute("tableau", tableau) ;
         // On transmet la liste des artistes pour pouvoir choisir l'auteur du tableau
         model.addAttribute("artistes", artisteDAO.findAll());
         return "formulaireTableau";
